42bd584c3b1869b85534cbefdf7d8ebc7b58fdb5
Andrei Jiménez <andrei_jg@outlook.com>
2024-05-15 09:24:29 -0600

    Works!!!

--- main.py
+++ main.py
@@ -2,33 +2,29 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.utils import platform
-from android.permissions import request_permissions, Permission
-import time
-import os
-from PIL import Image
-import cv2
-from io import BytesIO
-from kivy.core.image import Image as CoreImage
+from kivy.uix.widget import Widget
+from kivy.core.image import Image
+import numpy as np
 
-request_permissions([
-    Permission.CAMERA,
-    Permission.WRITE_EXTERNAL_STORAGE,
-    Permission.READ_EXTERNAL_STORAGE
-])
+import time, os, cv2
 
-Builder.load_string('''
+angle = 0
+if platform == 'android':
+    angle = -90
+
+Builder.load_string(f'''
 <CameraClick>:
     orientation: 'vertical'
     Camera:
         index: 0
         id: camera
-        resolution: (640,480)
+        resolution: (1920,1080)
         play: True
         allow_stretch: True
         canvas.before:
             PushMatrix
             Rotate:
-                angle: -90
+                angle: {angle}
                 origin: self.center
         canvas.after:
             PopMatrix
@@ -41,15 +37,11 @@ Builder.load_string('''
 
 
 class CameraClick(BoxLayout):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
+    def save_picture(self) -> str:
         timestr = time.strftime("%Y%m%d_%H%M%S")
         file_name = "IMG_{}.png".format(timestr)
         if platform == 'android':
@@ -58,29 +50,46 @@ class CameraClick(BoxLayout):
             file_path = os.path.join(dcim_path, file_name)
         else:
             file_path = file_name  # Si no es Android, solo usa el nombre del archivo
-        camera.export_to_png(file_path)
-        print("Imagen guardada en:", file_path)
 
-        # Leer la imagen con OpenCV
-        image = cv2.imread(file_path)
+        return file_path
+        
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        file_name = self.save_picture()
 
-        # Realizar operaciones de procesamiento de imagen con OpenCV
-        # Por ejemplo, puedes convertir la imagen a escala de grises
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        ### 1. Image - Con calidad ###            
 
-        # Mostrar la imagen procesada utilizando Kivy
-        buf = BytesIO()
-        Image.fromarray(gray_image).save(buf, format='png')
-        buf.seek(0)
-        img = CoreImage(BytesIO(buf.read()), ext='png')
-        # Muestra la imagen procesada
-        # Puedes usarla en un widget de imagen Kivy o guardarla como una imagen Kivy
-        # image_widget.texture = img.texture
+        image_from_camera = Widget.export_as_image(camera)
+        # img = Image(image_from_camera)
+        # img.save(f'{file_name}original_.png')
+
+
+        height, width = image_from_camera.texture.height, image_from_camera.texture.width
+        print(height, width)
+        # 690 1000
+        
+        newvalue = np.frombuffer(image_from_camera.texture.pixels, np.uint8)
+        newvalue = newvalue.reshape(height, width, 4)
+        gray = cv2.cvtColor(newvalue, cv2.COLOR_RGBA2GRAY)
+
+        aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_1000)
+        parameters = cv2.aruco.DetectorParameters_create()
+        corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+
+        if corners:
+            print("OK")
+
+            cv2.aruco.drawDetectedMarkers(gray, corners, ids)
+            backtorgb = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+            cv2.imwrite(file_name, backtorgb)
 
 class TestCamera(App):
 
     def build(self):
         return CameraClick()
-
 
 TestCamera().run()
