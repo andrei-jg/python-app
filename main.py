e262d5d890f3055b25c20665597731ba807a2fcc
Andrei Jiménez <andrei_jg@outlook.com>
2024-05-17 16:06:55 -0600

    Adding gitignore and setting new config files

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
 from kivy.utils import platform
-from kivy.uix.widget import Widget
-from kivy.core.image import Image
-import numpy as np
-
-import time, os, cv2
+from kivy_app import MyApp
 
 angle = 0
+resolution = "(1920, 1080)"
 if platform == 'android':
     angle = -90
 
 Builder.load_string(f'''
-<CameraClick>:
+<MyLayout>:
     orientation: 'vertical'
     Camera:
-        index: 0
         id: camera
-        resolution: (1920,1080)
+        resolution: {resolution}
         play: True
-        allow_stretch: True
         canvas.before:
             PushMatrix
             Rotate:
@@ -28,68 +21,9 @@ Builder.load_string(f'''
                 origin: self.center
         canvas.after:
             PopMatrix
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
+    ProcessedFrame:
+        id: processed_frame
 ''')
 
-
-class CameraClick(BoxLayout):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def save_picture(self) -> str:
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        file_name = "IMG_{}.png".format(timestr)
-        if platform == 'android':
-            # Obtener la ruta de la carpeta DCIM
-            dcim_path = os.path.join(os.environ['EXTERNAL_STORAGE'], 'DCIM')
-            file_path = os.path.join(dcim_path, file_name)
-        else:
-            file_path = file_name  # Si no es Android, solo usa el nombre del archivo
-
-        return file_path
-        
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        file_name = self.save_picture()
-
-        ### 1. Image - Con calidad ###            
-
-        image_from_camera = Widget.export_as_image(camera)
-        # img = Image(image_from_camera)
-        # img.save(f'{file_name}original_.png')
-
-
-        height, width = image_from_camera.texture.height, image_from_camera.texture.width
-        print(height, width)
-        # 690 1000
-        
-        newvalue = np.frombuffer(image_from_camera.texture.pixels, np.uint8)
-        newvalue = newvalue.reshape(height, width, 4)
-        gray = cv2.cvtColor(newvalue, cv2.COLOR_RGBA2GRAY)
-
-        aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_1000)
-        parameters = cv2.aruco.DetectorParameters_create()
-        corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-
-        if corners:
-            print("OK")
-
-            cv2.aruco.drawDetectedMarkers(gray, corners, ids)
-            backtorgb = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
-            cv2.imwrite(file_name, backtorgb)
-
-class TestCamera(App):
-
-    def build(self):
-        return CameraClick()
-
-TestCamera().run()
+if __name__ == "__main__":
+    MyApp().run()
---
+++ views/utils/new_login.py
@@ -0,0 +1,106 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+
+kv = """
+Screen:
+    MDCard:
+        size_hint: 0.9, None  # Adjust the width relative to the screen size
+        height: self.minimum_height  # Automatically adjust the height according to the content
+        pos_hint: {'center_x': 0.5, 'center_y': 0.55}
+        elevation: 10
+        padding: '10dp'  # Use dp to ensure a consistent size across different resolutions
+        spacing: '10dp'
+        orientation: 'vertical'
+
+        Image:
+            source: 'views/images/vrLogo.png'
+            size_hint: None, None
+            size: '170dp', '170dp'  # Adjust size as needed
+            pos_hint: {'center_x': 0.5}
+
+        MDLabel:
+            id: welcome_label
+            text: 'DSAASDA'
+            font_size: '32sp'  # Use sp for font size for scalability
+            halign: 'center'
+            size_hint_y: None
+            height: self.texture_size[1]
+            padding_y: '-3dp'
+            bold: True
+
+        MDLabel:
+            
+            text: "Clásica"
+            halign: 'right'  # Alinea el texto horizontalmente al centro
+            size_hint_y: None  # Fuerza al MDLabel a tener una altura específica
+            size_hint_x: 0.7
+            height: self.texture_size[1]  # Ajusta la altura basándose en el tamaño del texto
+            paddig_x: '10dp'
+            padding_y: '-3dp'  # Ajusta el espacio vertical entre la imagen y el texto
+
+        Widget:
+            size_hint_y: None
+            height: '10dp'  # Spacer adjusted to a relative size
+
+        MDTextField:
+            mode: 'round'
+            id: user
+            hint_text: 'Correo'
+            icon_right: 'account'
+            size_hint: 0.95, None  # Adjust the size relatively
+            height: '48dp'
+            font_size: '16sp'
+            pos_hint: {'center_x': 0.5}
+
+        Widget:
+            size_hint_y: None
+            height: '10dp'  # Spacer adjusted to a relative size
+
+        MDTextField:
+            mode: 'round'
+            id: password
+            hint_text: 'Contraseña'
+            icon_right: 'eye-off'
+            size_hint: 0.95, None  # Adjust the size relatively
+            height: '48dp'
+            font_size: '16sp'
+            pos_hint: {'center_x': 0.5}
+            password: True
+
+        Widget:
+            size_hint_y: None
+            height: '20dp'  # Spacer adjusted to a relative size
+
+        MDBoxLayout:
+            size_hint_y: None
+            height: self.minimum_height
+            size_hint_x: 1.05
+            spacing: '10dp'
+            pos_hint: {'center_x': 0.5}
+            padding: '10dp'
+            orientation: 'horizontal'
+
+            MDRoundFlatButton:
+                text: 'Iniciar Sesión'
+                font_size: '14sp'
+                size_hint: 0.3, None  # Adjust the size relatively
+                size_hint_y: None  # Use size_hint_y to allow height scaling
+                height: '48dp'
+
+            MDRoundFlatButton:
+                text: 'Olvidé mi cuenta'
+                font_size: '14sp'
+                size_hint: 0.3, None  # Adjust the size relatively
+                size_hint_y: None  # Use size_hint_y to allow height scaling
+                height: '48dp'
+"""
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_string(kv)
+
+if __name__ == "__main__":
+    MainApp().run()
